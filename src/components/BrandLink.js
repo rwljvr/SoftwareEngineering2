048ac60c2b2aa8e1ee017b02d0dd21445901065f
RAQUEL JAVIER - 4CS A <raquelhiraojavier@gmail.com>
2025-10-01 14:47:59 +0800

    final output for the prelim required revision

--- src/components/BrandLink.js
+++ src/components/BrandLink.js
@@ -1,16 +1,1 @@
-// src/components/BrandLink.js
-import React from "react";
 
-function BrandLink({ brandID, brandName, brandURL }) {
-  function openBrandSite() {
-    window.open(brandURL, "_blank");
-  }
-
-  return (
-    <div className="brand-link">
-      <button onClick={openBrandSite}>{brandName}</button>
-    </div>
-  );
-}
-
-export default BrandLink;
