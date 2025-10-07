3a767ade7464cb6a0eac9f5c3bdfb4e7a2df75a0
RAQUEL JAVIER - 4CS A <raquelhiraojavier@gmail.com>
2025-10-07 18:57:05 +0800

    fixed layout

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import HomePage from "./components/HomePage";
 import AboutPage from "./components/AboutPage";
 import OriginsPage from "./components/OriginsPage";   // ✅ new
 import MerchPage from "./components/MerchPage";       // ✅ new
-<<<<<<< HEAD
 import "./App.css";
 function App() {
   return (
@@ -18,20 +17,6 @@ function App() {
   </div>
 </div>
 
-=======
-
-function App() {
-  return (
-    <div className="app">
-      <Sidebar />
-      <div className="main-content">
-        <HomePage />
-        <AboutPage />
-        <OriginsPage />   {/* ✅ linked to "origins" */}
-        <MerchPage />     {/* ✅ linked to "merch" */}
-      </div>
-    </div>
->>>>>>> 048ac60c2b2aa8e1ee017b02d0dd21445901065f
   );
 }
 
